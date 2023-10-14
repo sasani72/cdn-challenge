de87a94d0ba15e7bdb6454b1dcfdc09ec4853c92
sasani <sasani_f72@yahoo.com>
2023-10-14 21:55:42 +0330

    admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'role' => 'admin',
+            'mobile' => '09120000000'
+        ]);
+    }
+}
